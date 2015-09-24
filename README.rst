c58bb6770491bf0d732bc7ef268820df83306e4c
messense <messense@icloud.com>
2015-09-24 14:21:28 +0800

    Update README.md and docs

--- README.rst
+++ README.rst
@@ -4,13 +4,13 @@ django-subdomains
 Subdomain helpers for the Django framework, including subdomain-based URL
 routing and reversing.
 
-Full documentation can be found here: http://django-subdomains.readthedocs.org/
+Full documentation can be found here: http://djsubdomains.readthedocs.org/
 
 Build Status
 ------------
 
-.. image:: https://travis-ci.org/messense/django-subdomains.svg?branch=master
-   :target: http://travis-ci.org/messense/django-subdomains
+.. image:: https://travis-ci.org/messense/djsubdomains.svg?branch=master
+   :target: http://travis-ci.org/messense/djsubdomains
 
 Tested on Python 2.6, 2.7, 3.3 and 3.4, as well as Django 1.5, 1.6, 1.7, and 1.8.
 
--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,5 @@
-import sys, os
+import sys
+import os
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 from subdomains import __version__
@@ -16,35 +17,39 @@ source_suffix = '.rst'
 master_doc = 'index'
 exclude_patterns = ['_build']
 
-project = u'django-subdomains'
+project = u'djsubdomains'
 copyright = u'2012, ted kaemming'
 version = release = '.'.join(map(str, __version__))
 
 html_static_path = ['_static']
-htmlhelp_basename = 'django-subdomainsdoc'
+htmlhelp_basename = 'djsubdomainsdoc'
 
 latex_elements = {}
 latex_documents = [
-  ('index', 'django-subdomains.tex', u'django-subdomains Documentation',
-   u'ted kaemming', 'manual'),
+    ('index', 'djsubdomains.tex', u'djsubdomains Documentation',
+     u'ted kaemming', 'manual'),
 ]
 
 man_pages = [
-    ('index', 'django-subdomains', u'django-subdomains Documentation',
+    ('index', 'djsubdomains', u'djsubdomains Documentation',
      [u'ted kaemming'], 1)
 ]
 
 texinfo_documents = [
-  ('index', 'django-subdomains', u'django-subdomains Documentation',
-   u'ted kaemming', 'django-subdomains', 'One line description of project.',
-   'Miscellaneous'),
+    ('index', 'djsubdomains', u'djsubdomains Documentation',
+     u'ted kaemming', 'djsubdomains', 'One line description of project.',
+     'Miscellaneous'),
 ]
 
 intersphinx_mapping = {
-    'python': ('http://docs.python.org/release/%s.%s' % sys.version_info[:2], None),
-    'django': ('http://docs.djangoproject.com/en/%s.%s/' % django.VERSION[:2],
+    'python': (
+        'http://docs.python.org/release/%s.%s' % sys.version_info[:2],
+        None
+    ),
+    'django': (
+        'http://docs.djangoproject.com/en/%s.%s/' % django.VERSION[:2],
         'http://docs.djangoproject.com/en/%s.%s/_objects/' % django.VERSION[:2]),
-}
+    }
 
 autodoc_member_order = 'bysource'
 autodoc_default_flags = ('members',)
